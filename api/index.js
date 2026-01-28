1734000bab394e8d23838493975310922b0e08ff
RaunaKSharma-err <progritik@gmail.com>
2026-01-28 20:49:06 +0545

    deployment ready

--- api/index.js
+++ api/index.js
@@ -18,5 +18,7 @@ app.get("/", (req, res) => {
   res.status(200).json({ message: "Good Health" });
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+if (process.env.NODE_ENV !== "production") {
+  app.listen(5000, () => console.log("Server running on port 5000"));
+}
 module.exports = app;
